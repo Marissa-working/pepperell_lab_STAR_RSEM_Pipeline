dfed7ee6941fc202ea59de1370caba3080a89ed1
Marissa Zhang <yzhang2587@wisc.edu>
2024-08-23 10:38:17 -0500

    create new pipeline of RSEM

--- samtools_sort.sh
+++
@@ -1,3 +0,0 @@
-gunzip -c $1.bam.gz > $1.bam
-samtools sort -O bam -T $1.sort $1.bam -o $1.sort.bam
-gzip $1.sort.bam 
--- samtools_view.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#samtools index $1.sam
-gunzip -c $1.sam.gz > $1.sam
-samtools view -bhSu $1.sam | gzip > $1.bam.gz

b2a75afeeed5367cef9ead2bd69ebd26545247fa
Marissa Zhang <yzhang2587@wisc.edu>
2024-11-01 17:04:50 -0500

    fix the error of shell variable

--- RSEM_STAR.sh
+++ RSEM_STAR.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Note: Shell variables assignment cannot have space
-$RSEMgenomeDir=testing
-$STARgenomeDir=star
-$ANNOTATION=MtbNCBIH37Rv_ncRNAs_sORFs.gtf
-$GENOME=MtbNCBIH37Rv.fa
-$threads=8
-$nThreadsSTAR=8
-$read1=3151_18_S12_R1_001.fastq.gz
-$read2=3151_18_S12_R2_001.fastq.gz
+RSEMgenomeDir=testing
+STARgenomeDir=star
+ANNOTATION=MtbNCBIH37Rv_ncRNAs_sORFs.gtf
+GENOME=MtbNCBIH37Rv.fa
+threads=8
+nThreadsSTAR=8
+read1=3151_18_S12_R1_001.fastq.gz
+read2=3151_18_S12_R2_001.fastq.gz
 
 # Replace the space in gtf file with underline
 python3 clear_space.py $ANNOTATION
